a2d11b4dfa26823c47f4628b678ce5173939cdd8
Nicholas Amancio <nick.amancio44@gmail.com>
2024-04-11 16:04:03 -0700

    removed the play_game method and combined it with the socket_connection method. Once the socket has been connected, the game can just run and send the board from server to client or vice-versa. Conditional statements were adjusted if one side wins

--- tictactoe.py
+++ tictactoe.py
@@ -1,35 +1,89 @@
 #imports
 import socket, json
 
-#method to play the game and to manipulate the board's position
-def play_game(board):
+#method to bind the server side to the client side. This starts the game
+def socket_bind_server_play_game():
+    #try block to create a server socket
+    try:
+        #create a socket binding specifying the type of socket to be created and the address family
+        #AF_INET = communicate over IPv4 networks
+        #SOCK_STREAM = type of socket to be created, creates a two-way connction-based type stream
+        #              makes sure that data is delivered in order without any errors or duplications
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("socket created successfully")
+    except socket.error as error:
+        print("socket creation failed, {}".format(error))
+
+    #binds the socket with the hostname of the local machine and the port number (random port number)
+    serversocket.bind((socket.gethostname(), 8080))
+    #listens for any client response
+    serversocket.listen(5)
+    #Once a client has been found, accept the client and connect the two together
+    clientsocket, address = serversocket.accept()
+    print("connection from {} has been established".format(address))
+    clientsocket.send(bytes("Welcome to the Server", "utf-8"))
+
+    #initalize the starting position of the board
+    board = [['', '', ''],['', '', ''],['', '', '']]
     #server plays as O (player 2)
     current_player = 'O'
-
-    #print the board
     print_board(board)
-    #make the server make a choice on where to play
-    player_move(current_player, board) 
-    #check for a winner after the server makes a move
-    winner_found = check_winner(board)
+    print("Waiting for Client's Move...")
 
-    #conditional statement to see what to return
-    #if server wins, declare server as winner and return a "close" statement to tell the socket to close
-    if winner_found:
+    #while the connection is secured, run a while loop to play the game
+    while True:
+        #since the server goes second, we listen for the client's message first
+        data_from_client = clientsocket.recv(1024)
+        #load board from json module
+        data = json.loads(data_from_client.decode())
+        #store into a 2D array variable
+        board = data.get("board")
+
+        #check to see if the server won in order to display it to the client, otherwise continue with the game
+        winner_found = check_winner(board)
+        if winner_found:
+            print_board(board)
+            print("You lost! Player X won")
+            break
+        elif check_if_full(board):
+            print_board(board)
+            print("It is a tie")
+            break
+
+        #print the board
         print_board(board)
-        declare_winner(current_player)
-        return "close"
-    #if the board is full, declare a tie and return a "close" statement to tell the socket to close
-    elif check_if_full(board):
-        print_board(board)
-        print("It is a tie")
-        return "close"
-    #otherwise print the board and return the updated board
-    else:
-        print_board(board)
-        print("Waiting for Client's Move...")
-        return board
-#end play_game
+        #make the server make a choice on where to play
+        player_move(current_player, board) 
+        #check for a winner after the server makes a move
+        winner_found = check_winner(board)
+
+        #conditional statement to see what to return
+        #if server wins, declare server as winner and return a "close" statement to tell the socket to close
+        if winner_found:
+            print_board(board)
+            declare_winner(current_player)
+            client_data = json.dumps({"board": board})
+            clientsocket.send(client_data.encode())
+            break
+        #if the board is full, declare a tie and return a "close" statement to tell the socket to close
+        elif check_if_full(board):
+            print_board(board)
+            print("It is a tie")
+            client_data = json.dumps({"board": board})
+            clientsocket.send(client_data.encode())
+            break
+        #otherwise print the board and send to the client
+        else:
+            print_board(board)
+            print("Waiting for Client's Move...")
+            #send the updated response back to the client
+            server_data = json.dumps({"board": board})
+            clientsocket.send(server_data.encode())
+
+    #Once the while loop breaks, close the connection for the server and client
+    clientsocket.close()
+    serversocket.close()
+#end socket_bind_server
 
 #method to print the board to the server
 def print_board(board):
@@ -93,50 +147,5 @@ def declare_winner(current_player):
     print("{} wins the game".format(current_player))
 #end declare_winner
 
-#method to bind the server side to the client side. This starts the program
-def socket_bind_server():
-    #try block to create a server socket
-    try:
-        #create a socket binding specifying the type of socket to be created and the address family
-        #AF_INET = communicate over IPv4 networks
-        #SOCK_STREAM = type of socket to be created, creates a two-way connction-based type stream
-        #              makes sure that data is delivered in order without any errors or duplications
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("socket created successfully")
-    except socket.error as error:
-        print("socket creation failed, {}".format(error))
-
-    #binds the socket with the hostname of the local machine and the port number (random port number)
-    serversocket.bind((socket.gethostname(), 8080))
-    #listens for any client response
-    serversocket.listen(5)
-    #Once a client has been found, accept the client and connect the two together
-    clientsocket, address = serversocket.accept()
-    print("connection from {} has been established".format(address))
-    clientsocket.send(bytes("Welcome to the Server", "utf-8"))
-
-    #while the connection is secured, run a while loop to play the game
-    while True:
-        #since the server goes second, we listen for the client's message first
-        data_from_client = clientsocket.recv(1024)
-        #conditional statement to see if the data from the client is "close" or a board position
-        if data_from_client.decode() == "close":
-            break
-        #if it is a board position, load from json module
-        data = json.loads(data_from_client.decode())
-        #store into a 2D array variable
-        board = data.get("board")
-
-        #play the game with the board sent from the client, this will return an updated board or "close"
-        server_board = play_game(board)
-        #send the updated response back to the client
-        server_data = json.dumps({"board": server_board})
-        clientsocket.send(server_data.encode())
-
-    #Once the while loop breaks, close the connection for the server and client
-    clientsocket.close()
-    serversocket.close()
-#end socket_bind_server
-
 #start socket_bind_server
-socket_bind_server()
+socket_bind_server_play_game()
